d5b4136c0b8363a0395a7546d67752a78b356bf8
Newms34 <Newms3450@msn.com>
2015-09-26 15:11:06 -0400

    temp

--- mel.js
+++ mel.js
@@ -1,32 +1,43 @@
 var app = angular.module("modeler", []);
 
 app.controller("MainController", function($scope, $window, $compile) {
-    $scope.test = new Date();
+    $scope.notes=[400,500,200,1000];
+    $scope.fullNotes = [];
+    $scope.makeFullNotes = function(){
+        $scope.fullNotes = [];//clear our full notes arr
+        for (var i=0;i<$scope.notes.length;i++){
+            var xPos = Math.floor(1000*$scope.notes[i]/$scope.notes.length);
+            $scope.fullNotes.push({
+                x:2,
+                y:$scope.notes[i];
+            })
+        }
+    }
     // random note generator
-    $scope.generateNotes = function generateNotes (melodyLength) {
-		var nums = [];
-		var frequencies = {
-		    0: 261.63, // C
-		    1: 293.66,
-		    2: 329.63,
-		    3: 349.23,
-		    4: 392.00,
-		    5: 440.00,
-		    6: 493.88,
-		    7: 523.25 // C (next octave)
-		}
-		while (nums.length < melodyLength){
-			nums.push(generateNum());
-		}
-		var freqs = nums.map(function (num){
-			return frequencies[num];
-		})
-		return freqs;
-	}
+    $scope.generateNotes = function generateNotes(melodyLength) {
+        var nums = [];
+        var frequencies = {
+            0: 261.63, // C
+            1: 293.66,
+            2: 329.63,
+            3: 349.23,
+            4: 392.00,
+            5: 440.00,
+            6: 493.88,
+            7: 523.25 // C (next octave)
+        }
+        while (nums.length < melodyLength) {
+            nums.push(generateNum());
+        }
+        var freqs = nums.map(function(num) {
+            return frequencies[num];
+        })
+        return freqs;
+    }
 });
 
-function generateNum () {
-	var min = 0; // 0 = C (middle)
-	var max = 7; // 7 = C (next octave)
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+function generateNum() {
+    var min = 0; // 0 = C (middle)
+    var max = 7; // 7 = C (next octave)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
